f2262482c93b5a95b2812bbcc0146aa0ad32189f
Jesson Yo <Aphostrophy@gmail.com>
2021-01-21 13:49:09 +0700

    Cryptarithm ver 1

--- src/cryptarithmetic.cpp
+++ src/cryptarithmetic.cpp
@@ -4,21 +4,32 @@
 #include <vector>
 #include <string>
 #include <math.h>
+#include <iomanip>
 using namespace std;
 
 typedef std::map<char, int> BooleanMap;
 
-bool isEqual(){
-
-    return true;
-}
-
-int stringToInt(string str,BooleanMap variables){
+int subtituteOperandsToInt(string str,BooleanMap variables){
     int sum = 0;
     for(int i=0;i < str.size();i++){
-        sum += variables[str[i]]*pow(10,str.size()-i);
+        sum += variables[str[i]]*pow(10,str.size()-i-1);
     }
     return sum;
+}
+
+bool isValid(vector<string> operands,BooleanMap variables){
+    int sum=0;
+
+    for(int i=0;i<operands.size();i++){
+        if(operands[i][0] != '-' && variables[operands[i][0]]==0){ // If first letter of a word is zero we assume it's false
+            return false;
+        }
+        if(i<operands.size()-2){
+            sum += subtituteOperandsToInt(operands[i],variables);
+        }
+    }
+
+    return sum == subtituteOperandsToInt(operands[operands.size()-1], variables);
 }
 
 int main(){
@@ -44,22 +55,53 @@ int main(){
                     variables[c] = 0;
                 }
             }
-            variables[line[0]] = 1; //Assume first letter can't be zero
             //Erases useless symbols
             variables.erase('-');
             variables.erase('+');
         }
         cout << endl;
+        string source = "";
         for ( const auto &pair : variables ) {
             cout << pair.first << " : " << pair.second << "\n";
+            source.append(to_string(pair.second));
         }
+
+        cout << source << endl;
 
         operands[operands.size()-3].pop_back(); //Removes the + sign
         for(int i=0;i<operands.size();i++){
-            cout << operands[i] << " : " << stringToInt(operands[i],variables) << "\n";
+            cout << operands[i] << " : " << subtituteOperandsToInt(operands[i],variables) << "\n";
         }
-    }
-    
+
+        cout << "---------------" << endl;
+
+        int trueLength = source.size();
+
+        while(stoi(source)<pow(10,trueLength)){
+            int mapIndex=0;
+            for ( const auto &pair : variables ) {
+                variables[pair.first] = (int)source[mapIndex] - 48;
+                mapIndex++;
+            }
+            // for(int i=0;i<operands.size();i++){
+            //     cout << operands[i] << " : " << subtituteOperandsToInt(operands[i],variables) << "\n";
+            // }
+            if(isValid(operands,variables)){
+                cout << "FOUND IT" << endl;
+                break;
+            }
+            // cout << endl;
+            stringstream ss;
+            ss << std::setfill('0') << std::setw(trueLength) << to_string(stoi(source)+1);
+            source = ss.str();
+        }
+
+        for(int i=0;i<operands.size();i++){
+            cout << operands[i] << " : " << subtituteOperandsToInt(operands[i],variables) << "\n";
+        }
+
+    } // END OF ELSE STATEMENT
+
     arith_file.close();
     return 0;
 }
